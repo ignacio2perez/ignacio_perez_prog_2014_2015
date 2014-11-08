831daff3d5c18e7fee754626fd8590660072ddf7
ignacio2perez <ignacio.2perez@gmail.com>
2014-11-08 11:39:08 +0100

    System.out.print/println
    Signed-off-by: ignacio2perez <ignacio.2perez@gmail.com>

---
+++ ImprimirPorPantalla/src/ImprimirPorPantalla.java
@@ -0,0 +1,13 @@
+
+public class ImprimirPorPantalla {
+
+	public static void main(String[] args) {
+		System.out.print("Esta es una prueba sin linea");
+		System.out.println("Esta es una prueba con linea");
+		int i=5;
+		boolean fr=false;
+				System.out.println("El resultado es:"+fr  + " y el valor de i es:"+ i);
+
+	}
+
+}
